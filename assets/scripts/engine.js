e82e7d7675b74f0d8e5db47da3669fe32174dd29
Kevin Soares <kevinhosoaresdepaiva@gmail.com>
2026-08-14 10:21:16 -0300

    implements lives system
    The game will now be finish if the user clicks elsewhere the target 3 times. Next stop will be adding sounds.

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -4,16 +4,18 @@ const state = {
         enemy: document.querySelector(".enemy"),
         time: document.querySelector("#time"),
         score: document.querySelector("#score"),
+        lives: document.querySelector("#lives-left")
     },
     values: {
         timerId: null,
         enemyVel: 1000,
-        hitBox: 0,
+        hitBox: null,
         baseScore: 0,
         enemypos: 0,
         timer: 60,
         counter: 1000,
         countDownId: null,
+        lifeCounter: 3,
     }
 };
     function randomBox(){
@@ -35,21 +37,36 @@ const state = {
                 if (box.id === state.values.hitBox){
                     state.values.baseScore++;
                     state.view.score.textContent = state.values.baseScore;
-                    state.values.hitBox = null;
                     state.values.enemypos.classList.remove("enemy");
                     state.values.enemypos.classList.add("enemy-hit");
+                    state.values.hitBox = null;
+                }
+                else{
+                    state.values.lifeCounter--;
+                    state.view.lives.textContent = state.values.lifeCounter;
+                    stopGame()
                 }
             }) 
         })
     }
     function timeLeft(){
         state.values.timer --;
-        if(state.values.timer<=0){
+        state.view.time.textContent = state.values.timer;
+        stopGame()
+    }
+    function stopGame(){
+        if(state.values.timer<=0 || state.values.lifeCounter <=0){
             alert("Game over! Your score: " +state.values.baseScore);
             state.values.timer = 60;
+            state.values.baseScore = 0
+            state.values.lifeCounter = 3
+            state.view.lives.textContent = state.values.lifeCounter;
+            state.view.score.textContent = state.values.baseScore;
         }
-        state.view.time.textContent = state.values.timer;
+
     }
+    function gameOver(){
+        }
     function countDown(){
         state.values.countDownId = setInterval(timeLeft, state.values.counter)
     }
@@ -57,6 +74,7 @@ function main() {
     movement();
     listenerhitEnemy();
     countDown();
+    stopGame();
 }
 
 main();
